75c8fa44771e9812addc6825f5cf4d426d683781
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-01-08 19:49:02 -0300

    App06

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      nome: ''
+    }
+    this.pegaNome = this.pegaNome.bind(this);
+  }
+
+  pegaNome(texto){
+    if (texto.length > 0){
+      this.setState({nome:'Bem vindo ' + texto});
+    }else{
+      this.setState({nome:''});
+    }
+   
+  }
+
+  render(){  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input}
+      placeholder="Digite seu nome:"
+      underlineColorAndroid="transparent"
+      onChangeText={this.pegaNome}
+      >
+
+      </TextInput>
+      <Text style={styles.texto}>{this.state.nome}</Text>
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1
   },
-});
+  input:{
+    height:45,
+    borderWidth:1,
+    borderColor:'blue',
+    marginTop:50,
+    margin:10,
+    fontSize:20,
+    padding:10,
+  },
+  texto:{
+    textAlign:'center',
+    fontSize:30
+  }
+})
+
+export default App;
+
